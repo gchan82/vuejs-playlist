63aadcdb0075a9b440f4923b6d2f5d8b682cb344
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-11-04 11:07:20 -0800

    upload setup

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div>
+  <app-header></app-header>
+  <app-ninjas></app-ninjas>
+  <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './Component/Header.vue';
+import Footer from './Component/Footer.vue';
+import Ninjas from './Component/Ninjas.vue'
+
+export default {
+  components:{
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas
+  },
+  data() {
+    return {
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
