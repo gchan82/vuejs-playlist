aabce75c8368a1feb7dbae2bffff09ebe4271b10
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-11-04 18:18:55 -0800

    Tutorial #28 - Dynamic Components! <keep-alive> is awesome!

---
+++ src/Component/formTwo.vue
@@ -0,0 +1,29 @@
+
+<template>
+<div>
+<h1> Form Two</h1>
+
+</div>
+
+</template>
+
+<script>
+
+export default {
+  components:{
+  },
+  data(){
+    return{
+      counter: 0
+    }
+  },
+  methods:{
+
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
