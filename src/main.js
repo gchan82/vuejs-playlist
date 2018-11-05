d03431335648bbe137aa352b9c18424848349bdd
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-11-04 20:35:31 -0800

    End of Tutorial #35 - Filters

--- src/main.js
+++ src/main.js
@@ -4,10 +4,18 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
 
+//Filters
+Vue.filter('to-uppercase', function (value) {
+  return value.toUpperCase()
+})
+
+Vue.filter('snippet', function (value) {
+  return value.slice(0, 100) + '...';
+})
 // Custom directives
 Vue.directive('rainbow', {
   bind(el, binding, vnode) { // eslint-disable-line no-console
-    el.style.color = "#" + Math.random().toString().slice(2,8)
+    el.style.color = "#" + Math.random().toString().slice(2, 8)
   }
 })
 
@@ -18,7 +26,7 @@ Vue.directive('theme', {
     } else if (binding.value === 'narrow') {
       el.style.maxWidth = '560px'
     }
-    if(binding.arg == 'column'){
+    if (binding.arg == 'column') {
       el.style.background = '#ddd';
       el.style.padding = '20px';
     }
