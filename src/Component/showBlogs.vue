4b955b4125935442dad0cc0ac67915f4094f1fa7
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-11-04 20:41:26 -0800

    tutorial #36 filter search method

--- src/Component/showBlogs.vue
+++ src/Component/showBlogs.vue
@@ -1,7 +1,8 @@
 <template>
 <div v-theme:column="'narrow'" id="show-blogs">
   <h1>All Blog Articles</h1>
-  <div v-for="blog in blogs" class="single-blog">
+  <input type="text" v-model="search" placeholder="search blogs" />
+  <div v-for="blog in filteredBlogs" class="single-blog">
   <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
   <article>{{blog.body | snippet}}</article>
 </div>
@@ -15,7 +16,8 @@ export default {
   },
   data() {
     return {
-      blogs: []
+      blogs: [],
+      search: ''
     }
   },
   methods: {},
@@ -24,6 +26,13 @@ export default {
       this.blogs = data.body.slice(0, 10);
     })
 
+  },
+  computed: {
+    filteredBlogs: function(){
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search);
+      })
+    }
   }
 
 }
