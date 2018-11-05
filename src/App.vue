a9d6df7ed8cc2af3d2addbe36d3afe06be9d1c73
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-11-04 17:48:30 -0800

    Tutorial #27 Form Slots

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,40 @@
 
 <template>
   <div>
-  <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-  <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-  <app-footer v-bind:title="title"></app-footer>
+<form-helper>
+<div slot="form-header">
+<h3>This is the title of the form</h3>
+<p>Info about form</p>
+</div>
+<div slot="form-fields">
+<input type="text" placeholder="name" required />>
+<input type="password" placeholder="password" required />>
+</div>
+<div slot="form-controls">
+<button v-on:click="handleSubmit">Submit</button>
+</div>
+</form-helper>
   </div>
 </template>
 
 <script>
-import Header from './Component/Header.vue';
-import Footer from './Component/Footer.vue';
-import Ninjas from './Component/Ninjas.vue'
+import formHelper from './Component/formHelper.vue'
 
 export default {
   components:{
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninjas': Ninjas
+    'form-helper': formHelper
   },
   data() {
     return {
-      ninjas: [
-  {name: 'Ryu', speciality: 'Vue Components', show: false},
-  {name: 'Ryu2', speciality: 'Vue Components2', show: false},
-  {name: 'Ryu3', speciality: 'Vue Components3', show: false},
-  {name: 'Ryu4', speciality: 'Vue Components4', show: false},
-  {name: 'Ryu5', speciality: 'Vue Components5', show: false},
-  {name: 'Ryu6', speciality: 'Vue Components6', show: false},
-  {name: 'Ryu7', speciality: 'Vue Components7', show: false},
-      ],
-      title: "Vue Ninjas app.vue"
+title: 'dyanamic slot title'
     };
   },
   methods:{
-    updateTitle: function(updatedTitle){
-      this.title = updatedTitle;
+
     }
   }
-};
+
 </script>
+
 <style scoped>
 </style>
